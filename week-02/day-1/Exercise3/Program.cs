a6196c6a067e6a0c0e803c3a491d9ad1a0b4de1c
davebaranyai <davebaranyai@gmail.com>
2017-11-27 15:44:39 +0100

    finish exercise3

---
+++ week-02/day-1/Exercise3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Zsuzsa!");
+            Console.WriteLine("Hello Misa!");
+            Console.WriteLine("Hello Mark!");
+        }
+    }
+}
