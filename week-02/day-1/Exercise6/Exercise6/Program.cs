60252fd54de66f6315806068e1ccf158045ce141
davebaranyai <davebaranyai@gmail.com>
2017-11-27 17:24:26 +0100

    finished exercise6

--- week-02/day-1/Exercise6/Exercise6/Program.cs
+++ week-02/day-1/Exercise6/Exercise6/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace Exercise6
+namespace GreenFox
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // An average Green Fox attendee codes 6 hours daily
+            // The semester is 17 weeks long
+            int avghrsperday = 6;
+            int semesterweeks = 17;
+            //
+            // Print how many hours is spent with coding in a semester by an attendee,
+            // if the attendee only codes on workdays.
+            Console.WriteLine((semesterweeks * 5) * avghrsperday);
+            //
+            // Print the percentage of the coding hours in the semester if the average
+            // work hours weekly is 52
+            Console.WriteLine((double)(avghrsperday * 5) / 52 * 100);
+            Console.ReadLine();
         }
     }
 }
