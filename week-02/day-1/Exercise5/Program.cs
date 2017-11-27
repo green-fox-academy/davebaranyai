9301a3ae4208e517c2bdd663db7ecca27888cdae
davebaranyai <davebaranyai@gmail.com>
2017-11-27 16:30:51 +0100

    finish exercise5

--- week-02/day-1/Exercise5/Program.cs
+++ week-02/day-1/Exercise5/Program.cs
@@ -11,14 +11,15 @@ namespace GreenFox
             // Print the result of 13 added to 22
             Console.WriteLine(13 + 22);
             // Print the result of 13 substracted from 22
-
+            Console.WriteLine(22 - 13);
             // Print the result of 22 mutiplied by 13
-
+            Console.WriteLine(22 * 13);
             // Print the result of 22 divided by 13 (as a decimal fraction)
-
+            Console.WriteLine(22 / 13.0);
             // Print the integer part of 22 divided by 13
-
+            Console.WriteLine(22 / 13);
             // Print the reminder of 22 divided by 13
+            Console.WriteLine(22 % 13);
             Console.ReadLine();
         }
     }
