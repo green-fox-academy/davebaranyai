a0d99809193dc4db380834302f6a4b846a8a25cd
davebaranyai <davebaranyai@gmail.com>
2017-11-27 16:05:17 +0100

    finish exercise4

---
+++ week-02/day-1/Exercise4/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Baranyai David");
+            Console.WriteLine(30);
+            Console.Write(1.75);
+            Console.Read();
+        }
+    }
+}
