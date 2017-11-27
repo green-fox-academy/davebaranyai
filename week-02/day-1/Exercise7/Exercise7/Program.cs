2da5ca12829b351d4b3597dafcf0ef3d877827c5
davebaranyai <davebaranyai@gmail.com>
2017-11-27 17:30:30 +0100

    finish exercise7

---
+++ week-02/day-1/Exercise7/Exercise7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Store your favorite number in a variable (as a number)
+            // And print it like this: "My favorite number is: 8"
+            int favnumber = 11;
+            string greeting = "My favorite number is: ";
+            Console.WriteLine(greeting + favnumber);
+
+            Console.ReadLine();
+        }
+    }
+}
