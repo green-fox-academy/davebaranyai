907eb903f9662026e2f96ab4faa7e3f8b92199d4
davebaranyai <davebaranyai@gmail.com>
2017-11-27 19:51:48 +0100

    finish exercise 10

---
+++ week-02/day-1/Exercise10/Exercise10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define several things as a variable then print their values
+            // Your name as a string
+            // Your age as an integer
+            // Your height in meters as a double
+            // Whether you are married or not as a boolean
+
+            string name = "David Baranyai";
+            int age = 30;
+            double height = 1.75;
+            bool married = false;
+
+            Console.ReadLine();
+        }
+    }
+}
