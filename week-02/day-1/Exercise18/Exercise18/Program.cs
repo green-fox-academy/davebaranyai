692b647cb883eb528a8578e379e3ed7b133e37ed
davebaranyai <davebaranyai@gmail.com>
2017-11-28 08:20:45 +0100

    finish exercise18

---
+++ week-02/day-1/Exercise18/Exercise18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number form the standard input,
+            // Than prints "Odd" if the number is odd, or "Even" it it is even.
+            int number = 14;
+
+            if ((number % 2) == 0)
+            {
+                Console.WriteLine("EVEN");
+            }
+            else
+            {
+                Console.WriteLine("ODD");
+            }
+            Console.ReadLine();
+        }
+    }
+}
