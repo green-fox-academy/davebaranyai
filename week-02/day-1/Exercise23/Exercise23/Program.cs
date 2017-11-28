9d77097399b4b3b062067be6e7a444a76c422e7a
davebaranyai <davebaranyai@gmail.com>
2017-11-28 11:51:56 +0100

    feat(Exercise23)finish exercise

--- week-02/day-1/Exercise23/Exercise23/Program.cs
+++ week-02/day-1/Exercise23/Exercise23/Program.cs
@@ -8,9 +8,13 @@ namespace GreenFox
         {
             // Create a program that writes this line 100 times:
             // "I won't cheat on the exam!"
+            string cheat = "I won't cheat on the exam!";
+            for (int i = 1; i <= 99; i++)
+            {
+                Console.WriteLine(cheat);
+            }
 
-
-            Console.ReadLine();
+            Console.Read();
         }
     }
 }
