69f7c6e181bcb44a44f727921131bb5df5d1bdbe
davebaranyai <davebaranyai@gmail.com>
2017-11-28 08:31:30 +0100

    started exercise23

---
+++ week-02/day-1/Exercise23/Exercise23/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that writes this line 100 times:
+            // "I won't cheat on the exam!"
+
+
+            Console.ReadLine();
+        }
+    }
+}
