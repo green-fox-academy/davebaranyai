9834a8553a1e17b6ce1fe62d94eb42c3c55c61f2
davebaranyai <davebaranyai@gmail.com>
2017-11-28 15:37:17 +0100

    feat(AppendA): finish exercise

--- week-02/day-2/AppendA/AppendA/Program.cs
+++ week-02/day-2/AppendA/AppendA/Program.cs
@@ -10,7 +10,15 @@ namespace AppendA
             // - Write a function called `appendA` that gets a string as an input
             //   and appends an 'a' character to its end
             // - Print the result of `appendA(am)`
+
+            string am = "kuty";
+            appendA(am);
             Console.ReadLine();
+        }
+        static void appendA(string name)
+        {
+            Console.WriteLine(name + "a");
+      
         }
     }
 }
