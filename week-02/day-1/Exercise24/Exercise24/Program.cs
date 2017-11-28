3014d1ca076cd29b5329f875d2bf83d1de44a3fd
davebaranyai <davebaranyai@gmail.com>
2017-11-28 17:14:17 +0100

    feat(Exercise24): finish exercise

---
+++ week-02/day-1/Exercise24/Exercise24/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that prints all the even numbers between 0 and 500
+
+            for (int i = 2; i <= 500; )
+            {
+                Console.WriteLine(i);
+                i=i + 2;
+            }
+            Console.ReadLine();
+        }
+    }
+}
