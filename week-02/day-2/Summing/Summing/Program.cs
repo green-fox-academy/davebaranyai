bd697b80ef28a2e24750a91a13ddda09c56480c7
davebaranyai <davebaranyai@gmail.com>
2017-11-29 14:02:03 +0100

    feat(Exercise01): finish exercise

--- week-02/day-2/Summing/Summing/Program.cs
+++ week-02/day-2/Summing/Summing/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
-namespace Summing
+namespace Sum
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // - Write a function called `sum` that sum all the numbers
+            //   until the given parameter
+            int number = 12;
+            
+
+            Console.ReadLine();
+        }
+
+        static void sum(int numberstart)
+        {
+            for (int i= 0; i <= numberstart; numberstart++)
+            {
+
+            }
         }
     }
 }
