c4f54836b959838cfc2fd5d303cd5773b38bd706
davebaranyai <davebaranyai@gmail.com>
2017-11-29 16:22:53 +0100

    feat(Exercise04): exercise done

---
+++ week-02/day-3/Exercise04/Exercise04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TodoPrint
+{
+    public class TodoPrint
+    {
+        public static void Main(string[] args)
+        {
+            string todoText = " - Buy milk\n";
+            // Add "My todo:" to the beginning of the todoText
+            // Add " - Download games" to the end of the todoText
+            // Add " - Diablo" to the end of the todoText but with indention
+
+            // Expected output:
+
+            // My todo:
+            //  - Buy milk
+            //  - Download games
+            //      - Diablo
+
+            todoText = todoText.Insert(0, "My todo:\n");
+            todoText = todoText.Insert(21, " - Download games\n");
+            todoText = todoText.Insert(39, "\t - Diablo");
+            Console.WriteLine(todoText);
+            Console.ReadLine();
+        }
+    }
+}
