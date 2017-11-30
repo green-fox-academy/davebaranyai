b40fc2084dc0743e6ed3207003b43d60bfb7af03
davebaranyai <davebaranyai@gmail.com>
2017-11-30 10:30:11 +0100

    feat(Practice):file creation for practice reasons

---
+++ week-02/day-4/Practice/Practice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Swap the values of the variables
+            int a = 123;
+            int b = 526;
+
+            a = 526;
+            b = 123;
+
+            Console.WriteLine("a= " + a);
+            Console.WriteLine("b= " + b);
+            Console.ReadLine();
+        }
+    }
+}
