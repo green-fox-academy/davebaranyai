0a31a72c638a5b52d6fd6a6bcfe8e52be0610607
davebaranyai <davebaranyai@gmail.com>
2017-11-30 14:00:50 +0100

    feat(day 1_Exercise12-13): exercises done

---
+++ week-02/day-1/Exercise12/Exercise12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that stores 3 sides of a cuboid as variables (doubles)
+            // The program should write the surface area and volume of the cuboid like:
+            //
+            // Surface Area: 600
+            // Volume: 1000
+
+            double side1 = 8.0;
+            double side2 = 11.0;
+            double side3 = 9.0;
+
+            double area = 2 * ((side1 * side2) + (side2 * side3) + (side1 * side3));
+            double volume = side1 * side2 * side3;
+
+            Console.WriteLine("Surface Area: " + area);
+            Console.WriteLine("Volume: " + volume);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ week-02/day-1/Exercise13/Exercise13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 14;
+            int currentMinutes = 34;
+            int currentSeconds = 42;
+
+            // Write a program that prints the remaining seconds (as an integer) from a
+            // day if the current time is represented bt the variables
+
+            int currentAllSecs = (currentHours * 60 * 60) + (currentMinutes * 60) + currentSeconds;
+            int dayAllSecs = 24 * 60 * 60;
+
+            int remaining = (dayAllSecs - currentAllSecs);
+
+            Console.WriteLine("Reamining seconds from the day: " + remaining);
+            Console.ReadLine();
+        }
+    }
+}
