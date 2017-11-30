11c831355e607d21a247c76355e8e5db06c699fb
davebaranyai <davebaranyai@gmail.com>
2017-11-30 23:28:15 +0100

    feat(day1_Exercise12): added user input feature and 2 more options

---
+++ week-02/day-1/Exercise12/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write the length of side 'a' in cm: ");
+            double sideA = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please write the length of side 'b' in cm: ");
+            double sideB = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please write the length of side 'c' in cm: ");
+            double sideC = double.Parse(Console.ReadLine());
+
+            double area = 2 * ((sideA * sideB) + (sideB * sideC) + (sideA * sideC));
+            double volume = sideA * sideB * sideC;
+
+            Console.WriteLine("Surface Area: " + area + " cm2");
+            Console.WriteLine("Volume: " + volume + " cm3");
+            Console.ReadLine();
+        }
+    }
+}
--- week-02/day-1/Exercise12/Exercise12/Program.cs
+++ week-02/day-1/Exercise12/Exercise12/Program.cs
@@ -6,23 +6,68 @@ namespace GreenFox
     {
         static void Main(string[] args)
         {
-            // Write a program that stores 3 sides of a cuboid as variables (doubles)
-            // The program should write the surface area and volume of the cuboid like:
-            //
-            // Surface Area: 600
-            // Volume: 1000
+            Console.WriteLine("Write '1' for cuboid, '2' for cylinder, or '3' for sphere:");
 
-            double side1 = 8.0;
-            double side2 = 11.0;
-            double side3 = 9.0;
+            int answer = Int16.Parse(Console.ReadLine());
 
-            double area = 2 * ((side1 * side2) + (side2 * side3) + (side1 * side3));
-            double volume = side1 * side2 * side3;
+            if (answer == 1)
+            {
+                CuboidMethod();
+            }
+            else if (answer == 2)
+            {
+                CylinderMethod();
+            }
+            else if (answer == 3)
+            {
+                SphereMethod();
+            }
+            else
+            {
+                Console.WriteLine("Please, learn how to read.");
+            }                   
+        }
+        static void CuboidMethod()
+        {
+            Console.WriteLine("Please write the length of side 'a' in cm: " );
+            double sideA = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please write the length of side 'b' in cm: ");
+            double sideB = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please write the length of side 'c' in cm: ");
+            double sideC = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Surface Area: " + area);
-            Console.WriteLine("Volume: " + volume);
+            double area = 2 * ((sideA * sideB) + (sideB * sideC) + (sideA * sideC));
+            double volume = sideA * sideB * sideC;
+
+            Console.WriteLine("Surface Area: " + area + " cm2");
+            Console.WriteLine("Volume: " + volume + " cm3");
             Console.ReadLine();
         }
+        static void CylinderMethod()
+        {
+            Console.WriteLine("Please write the height of the cylinder in cm: ");
+            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please write the length of the radius (half of the diameter) of the cylinder in cm: ");
+            double radius = double.Parse(Console.ReadLine());            
+        
+            double area = (2 * height * radius * Math.PI) + (2 * Math.Pow(radius, 2) * Math.PI);
+            double volume = height * Math.Pow(radius, 2) * Math.PI;
 
+            Console.WriteLine("Surface Area: " + area + " cm2");
+            Console.WriteLine("Volume: " + volume + " cm3");
+            Console.ReadLine();
+        }
+        static void SphereMethod()
+        {
+            Console.WriteLine("Please write the length of the radius (half of the diameter) of the sphere in cm: ");
+            double radius = double.Parse(Console.ReadLine());
+
+            double area = 4 * Math.Pow(radius, 2) * Math.PI;
+            double volume = (4 * Math.Pow(radius, 3) * Math.PI) / 3;
+
+            Console.WriteLine("Surface Area: " + area + " cm2");
+            Console.WriteLine("Volume: " + volume + " cm3");
+            Console.ReadLine();
+        }
     }
 }
