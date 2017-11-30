d64d2643ae77723497144cc044ed169e81b42c4a
davebaranyai <davebaranyai@gmail.com>
2017-11-30 10:48:58 +0100

    feat(day1 - Exercise9)exercise done

---
+++ week-02/day-1/Exercise9/Exercise9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the Body mass index (BMI) based on these values
+            double massInKg = 81.2;
+            double heightInM = 1.78;
+           
+
+            double BMI = (massInKg / Math.Pow(heightInM, 2));
+            Console.WriteLine(BMI);
+            Console.ReadLine();
+        }
+    }
+}
