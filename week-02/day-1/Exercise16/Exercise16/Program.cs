4280d1e672f3576bedefbf904e395336b8ac0b0c
davebaranyai <davebaranyai@gmail.com>
2017-11-30 16:21:16 +0100

    feat(day1_exercise16): exercise done

---
+++ week-02/day-1/Exercise16/Exercise16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two integers
+            // The first represents the number of chickens the farmer has
+            // The seconf represents the number of pigs the farmer has
+            // It should print how many legs all the animals have
+            Console.WriteLine("How many chickens do you have?");
+            int chick = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many pigs do you have?");
+            int pig = Int32.Parse(Console.ReadLine());
+
+            int legstotal = (chick * 2) + (pig * 4); 
+            Console.WriteLine("Total legs of your animals: " + legstotal);
+
+            Console.ReadLine();
+        }
+    }
+}
