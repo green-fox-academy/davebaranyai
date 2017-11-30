3ac8a3d2a0f67533e6b7d4101dc756ecb620d636
davebaranyai <davebaranyai@gmail.com>
2017-11-30 15:54:38 +0100

    feat(day1_Exercise15): exercise done

---
+++ week-02/day-1/Exercise15/Exercise15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for an integer that is a distance in kilometers,
+            // then it converts that value to miles and prints it
+            Console.WriteLine("How many kilometres do you want to convert?");
+            double kilometers = Console.Read();
+            double miles = kilometers * 0.62137;
+            Console.WriteLine(kilometers + " kilometers are " + miles + " miles");
+            Console.ReadKey();
+        }
+    }
+}
