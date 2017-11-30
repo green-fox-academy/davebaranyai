b27713865c0dd167010354f09e5d954888d0b29e
davebaranyai <davebaranyai@gmail.com>
2017-11-30 16:07:44 +0100

    fix(day1_Exercise15) fixed string conversion

--- week-02/day-1/Exercise15/Exercise15/Program.cs
+++ week-02/day-1/Exercise15/Exercise15/Program.cs
@@ -9,10 +9,10 @@ namespace GreenFox
             // Write a program that asks for an integer that is a distance in kilometers,
             // then it converts that value to miles and prints it
             Console.WriteLine("How many kilometres do you want to convert?");
-            double kilometers = Console.Read();
-            double miles = kilometers * 0.62137;
+            string kilometers = Console.ReadLine();
+            double miles = Double.Parse(kilometers) * 0.62137;            
             Console.WriteLine(kilometers + " kilometers are " + miles + " miles");
-            Console.ReadKey();
+            Console.ReadLine();
         }
     }
 }
