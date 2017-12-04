dce95a16b807397c379e5b1c171c0258ef5680cf
davebaranyai <davebaranyai@gmail.com>
2017-12-04 16:40:13 +0100

    feat(Exercise03): first few steps

---
+++ week-03/day-1/Exercise03/Exercise03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Write a function that takes a filename as string,
+            // then returns the number of lines the file contains.
+            // It should return zero if it can't open the file, and
+            // should not raise any error.
+
+            string[] array1 = File.ReadAllLines(InputMove);
+        }
+        static string InputMove()
+        {
+            string Input = @"./my-file.txt";
+            return Input;
+        }
+    }
+    
+}
