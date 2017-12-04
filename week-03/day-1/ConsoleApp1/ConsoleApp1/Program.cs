cb902ad967c8b4b5baa08cdc269ed29bb5ced156
davebaranyai <davebaranyai@gmail.com>
2017-12-04 13:13:43 +0100

    feat(Exercise01): exercise done

---
+++ week-03/day-1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi a szam?");
+
+            try
+            {
+                int divisor = int.Parse(Console.ReadLine());
+                int result = 10 / divisor;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Fail");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
