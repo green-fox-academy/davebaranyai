3c74f246a222891637b41d6713862de43e99e7b1
davebaranyai <davebaranyai@gmail.com>
2017-12-04 14:34:40 +0100

    feat(Exercise02): exercise done

---
+++ week-03/day-1/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @".\my-file.txt";
+            try
+            {
+                string[] stuff = File.ReadAllLines(path);
+
+                foreach (var line in stuff)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (FileNotFoundException e)
+            {
+
+                Console.WriteLine("Unable to read file: my-file.txt");
+            }          
+
+            Console.ReadLine();
+        }
+    }
+}
