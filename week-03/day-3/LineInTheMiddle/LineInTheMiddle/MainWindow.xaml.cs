27867b86e421ff5294ffa91e23890114ce50f2b4
davebaranyai <davebaranyai@gmail.com>
2017-12-06 11:25:55 +0100

    feat(LineInTheMiddle): exercise done

---
+++ week-03/day-3/LineInTheMiddle/LineInTheMiddle/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace LineInTheMiddle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            MiddleLines(foxDraw);
+        }
+        public static void MiddleLines(FoxDraw foxDraw)
+        {
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(0, 175, 525, 175);
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(262.5, 0, 262.5, 350);
+
+
+        }
+    }
+}
