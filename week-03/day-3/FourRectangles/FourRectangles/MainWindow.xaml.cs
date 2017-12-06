402cb53890285858c481f736e75b22e9b03ea6a7
davebaranyai <davebaranyai@gmail.com>
2017-12-06 15:36:04 +0100

    feat(FourRectangles): exercise done

---
+++ week-03/day-3/FourRectangles/FourRectangles/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+
+namespace FourRectangles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            Random parameter = new Random();
+            FourRects(foxDraw, parameter);
+        }
+        public static void FourRects(FoxDraw foxDraw, Random parameter)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                int x = parameter.Next(1, 255);
+                int y = parameter.Next(1, 250);
+                int length = parameter.Next(20, 200);
+                int height = parameter.Next(20, 200);
+                byte RGB1 = Convert.ToByte(parameter.Next(1, 255));
+                byte RGB2 = Convert.ToByte(parameter.Next(1, 255));
+                byte RGB3 = Convert.ToByte(parameter.Next(1, 255));
+
+
+                foxDraw.FillColor(Color.FromRgb(RGB1, RGB2, RGB3));
+                foxDraw.DrawRectangle(x, y, length, height);
+
+            }
+
+        
+        }
+    }
+}
