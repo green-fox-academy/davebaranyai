1af036c8b57f313a21667b8edc56679bcebf9322
davebaranyai <davebaranyai@gmail.com>
2017-12-07 14:15:17 +0100

    feat(Exercise03): exercise done

---
+++ week-03/day-4/Exercise03/Exercise03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int result = Sumdigit(number);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        static int Sumdigit(int number)
+        {
+            if (number < 10)
+            {
+                return number;
+            }
+            else
+            {
+                return (number % 10) + Sumdigit(number / 10);
+            }
+        }
+    }
+}
