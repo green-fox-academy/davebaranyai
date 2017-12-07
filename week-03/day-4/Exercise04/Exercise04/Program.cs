e92f5fc1ecbe8243c38d02033f173eca9dab2d31
davebaranyai <davebaranyai@gmail.com>
2017-12-07 14:28:49 +0100

    feat(Exercise04): exercise done

---
+++ week-03/day-4/Exercise04/Exercise04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Base:");
+            int basenum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("PowerN:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = Power(basenum, n);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        static int Power(int basenum, int n)
+        {
+            if (basenum == 0)
+            {
+                return 0;
+            }
+            else if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return basenum * Power(basenum, --n);
+            }
+        }
+    }
+}
