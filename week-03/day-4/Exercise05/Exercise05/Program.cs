287be197acdc0df600917fa7208809043b229758
davebaranyai <davebaranyai@gmail.com>
2017-12-07 15:17:52 +0100

    feat(Exercise05): exercise done

---
+++ week-03/day-4/Exercise05/Exercise05/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Exercise05
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Bunnies:");
+//            int bunnies = Convert.ToInt32(Console.ReadLine());
+//            int counter = 1;
+
+//            int result = EarsNum(bunnies, counter);
+//            Console.WriteLine(result);
+//            Console.ReadLine();
+//        }
+//        static int EarsNum(int bunnies, int counter)
+//        {
+//            if (counter > 1)
+//            {
+//                return bunnies;
+//            }
+//            else
+//            {
+//                return bunnies + EarsNum(bunnies, counter + 1);
+//            }
+//        }
+//    }
+//}
+
+namespace Exercise05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bunnies:");
+            int bunnies = Convert.ToInt32(Console.ReadLine());
+
+            int result = EarsNum(bunnies);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        static int EarsNum(int bunnies)
+        {
+            if (bunnies == 1)
+            {
+                return 2;
+            }
+            else
+            {
+                return 2 + EarsNum(bunnies - 1);
+            }
+        }
+    }
+}
