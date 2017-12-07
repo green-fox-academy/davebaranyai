06cd7b0a14cb90adbc3168ed47389b57b8c1838e
davebaranyai <davebaranyai@gmail.com>
2017-12-07 12:40:13 +0100

    feat(Exercise02): exercise done

---
+++ week-03/day-4/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int result = Numberadder (number);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        static int Numberadder(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number + Numberadder(number - 1);               
+            }
+        }
+    }
+}
