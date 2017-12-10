53ffac9466728f389230c8e0c7646020ce78684c
davebaranyai <davebaranyai@gmail.com>
2017-12-10 23:07:04 +0100

    feat(day2_Exercise28): exercise done

---
+++ week-03/day-4/Exercise10/Exercise10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which Fibonacci number would you want? Enter a number:");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The " + Number + "th Fibonacci number is: " + NthFibonacci(Number));
+            Console.ReadLine();
+        }
+        static int NthFibonacci(int Number)
+        {
+            if (Number == 0)
+            {
+                return 0;
+            }
+            else if (Number <= 2)
+            {
+                return 1;
+            }            
+            return NthFibonacci(Number - 1) + NthFibonacci(Number - 2);            
+        }
+    }   
+}
