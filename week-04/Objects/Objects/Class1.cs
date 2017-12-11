3cc9ad4b893a3da873961def976b1c5ae46f6d13
davebaranyai <davebaranyai@gmail.com>
2017-12-11 11:50:22 +0100

    feat(Objects practicing): exercise done

---
+++ week-04/Objects/Objects/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Objects
+{
+    class Tree
+    {
+        private int Leaf;
+        public int MyProperty { get; set; }
+
+    }
+}
