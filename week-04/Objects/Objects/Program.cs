8e612fde80ae2443e47c4c22cc2768bb5c80b007
davebaranyai <davebaranyai@gmail.com>
2017-12-11 12:36:22 +0100

    feat(Post-It): exercise done

--- week-04/Objects/Objects/Program.cs
+++ week-04/Objects/Objects/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Objects
 {
@@ -6,6 +7,7 @@ namespace Objects
     {
         static void Main(string[] args)
         {
+            var list = new List<int>() { 1, 2, 3, 4 };
             Console.WriteLine("Hello World!");
         }
     }
---
+++ week-04/Post-It/Post-It/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Post_It
+{
+    class Program
+    {
+        struct PostIt
+        {
+            public string BackgroundColor;
+            public string TextColor;
+            public string Text;
+
+        }
+        static void Main(string[] args)
+        {
+            PostIt postit1;
+            postit1.BackgroundColor = "Orange";
+            postit1.TextColor = "Blue";
+            postit1.TextColor = "Idea 1";
+
+            PostIt postit2;
+            postit2.BackgroundColor = "Pink";
+            postit2.TextColor = "Black";
+            postit2.TextColor = "Awesome";
+
+            PostIt postit3;
+            postit3.BackgroundColor = "Yellow";
+            postit3.TextColor = "Green";
+            postit3.TextColor = "Superb!";
+
+
+        }
+    }
+}
