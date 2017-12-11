9646f2c0ed96def8379f5e2248672aa208fb9c7b
davebaranyai <davebaranyai@gmail.com>
2017-12-11 16:57:03 +0100

    feat(Animal): created Animal class

---
+++ week-04/Animal/Animal/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal
+{
+    class Animal
+    {
+        public int Hunger { get; set; }
+        public int Thirst { get; set; }
+
+        public Animal()
+        {
+            Hunger = 50;
+            Thirst = 50;
+        }
+    }
+}
