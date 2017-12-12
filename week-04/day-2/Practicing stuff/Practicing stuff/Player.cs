800273c16a2a7a7bc650f7bc77ae506dabe3de33
davebaranyai <davebaranyai@gmail.com>
2017-12-12 16:30:11 +0100

    feat(SuchPractice): practicing some, etc.

---
+++ week-04/day-2/Practicing stuff/Practicing stuff/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practicing_stuff
+{
+    class Player
+    {
+        private int lives = 100;
+
+        public int Lives
+        {
+            get
+            {
+                return lives;
+            }
+            set
+            {
+                //if (value <= 0 || value >= 1000)
+                //{
+                //    Console.WriteLine("Makes no sense.");
+                //    return;
+                //}
+                lives = value;
+            }
+        }
+    }
+}
---
+++ week-04/day-2/Practicing stuff/Practicing stuff/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Practicing_stuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            player.Lives = 134;
+
+            Console.WriteLine(player.Lives);
+            Console.ReadLine();
+
+        }
+    }
+}
