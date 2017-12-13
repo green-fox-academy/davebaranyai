e30eb2cf32eccdcfe14b3519ccf638a8e814b972
davebaranyai <davebaranyai@gmail.com>
2017-12-13 09:13:16 +0100

    Fixed folder structure and other discrepancies

---
+++ week-04/day-1/Sharpie/Sharpie/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sharpie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sharpie1 = new Sharpie();
+
+            Console.WriteLine(sharpie1.Color = "orange");
+            Console.WriteLine(sharpie1.Width = 3.2);
+            Console.WriteLine("Ink Amount: " + sharpie1.InkAmount);
+            sharpie1.Use();
+            Console.WriteLine("Ink Amount after usage: " + sharpie1.InkAmount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-04/day-1/Sharpie/Sharpie/Sharpie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharpie
+{
+    class Sharpie
+    {
+        public string Color { get; set; }
+        public double Width { get; set; }
+        public double InkAmount { get; set; }
+        
+        public Sharpie()
+        {
+            InkAmount = 100;
+        }
+        public void Use()
+        {
+            InkAmount -= 1;
+        }
+    }
+}
