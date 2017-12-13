69f4b83141492bf8152debb3dae767b650d3b128
davebaranyai <davebaranyai@gmail.com>
2017-12-14 00:31:01 +0100

    feat(day1_Exercise25): exercise done

--- week-02/day-1/Exercise25/Exercise25/Program.cs
+++ week-02/day-1/Exercise25/Exercise25/Program.cs
@@ -6,29 +6,18 @@ namespace Exercise25
     {
         static void Main(string[] args)
         {
-            // Create a program that asks for a number and prints the multiplication table with that number
-            //
-            // Example:
-            // The number 15 should print:
-            //
-            // 1 * 15 = 15
-            // 2 * 15 = 30
-            // 3 * 15 = 45
-            // 4 * 15 = 60
-            // 5 * 15 = 75
-            // 6 * 15 = 90
-            // 7 * 15 = 105
-            // 8 * 15 = 120
-            // 9 * 15 = 135
-            // 10 * 15 = 150
             Console.WriteLine("What is your number: ");
             int UserInput = Convert.ToInt32(Console.ReadLine());
+
             Multiplication(UserInput);
             Console.ReadLine();
         }
         static void Multiplication(int UserInput)
         {
-
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(i + " * " + UserInput + " = " + (i * UserInput));
+            }
         }
     }
 }
