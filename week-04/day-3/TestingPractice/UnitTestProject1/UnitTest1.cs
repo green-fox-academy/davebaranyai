2def2fc9a0517eff752cb475808983165ec4a99b
davebaranyai <davebaranyai@gmail.com>
2017-12-13 11:59:31 +0100

    feat(TestingPractice): practiced a bit - vol2

--- week-04/day-3/TestingPractice/UnitTestProject1/UnitTest1.cs
+++ week-04/day-3/TestingPractice/UnitTestProject1/UnitTest1.cs
@@ -1,12 +1,15 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit;
 using NUnit.Framework;
+using TestingPractice;
 
 namespace UnitTestProject1
 {
     [TestFixture]
     public class UnitTest1
     {
+        MathTask
+
         [Test]
         public void TestMethod1()
         {
