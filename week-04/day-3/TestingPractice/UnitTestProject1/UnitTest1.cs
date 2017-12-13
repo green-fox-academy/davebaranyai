5e4f120fab4d66576aec705fbd6ea860ee54857f
davebaranyai <davebaranyai@gmail.com>
2017-12-13 11:58:05 +0100

    feat(TestingPractice): practiced a bit

---
+++ week-04/day-3/TestingPractice/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit;
+using NUnit.Framework;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
