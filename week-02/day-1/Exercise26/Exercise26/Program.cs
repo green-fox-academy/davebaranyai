4f52b5ff1253ccb9526719335b6b5586735f456b
davebaranyai <davebaranyai@gmail.com>
2017-12-14 00:43:48 +0100

    feat(day1_Exercise26): main method done, added more methods

---
+++ week-02/day-1/Exercise26/Exercise26/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercise26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First number: ");
+            int Number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second number: ");
+            int Number2 = Convert.ToInt32(Console.ReadLine());
+            Comparing(Number1, Number2);
+            // Create a program that asks for two numbers
+            // If the second number is not bigger than the first one it should print:
+            // "The second number should be bigger"
+            //
+            // If it is bigger it should count from the first number to the second by one
+            //
+            // example:
+            //
+            // first number: 3, second number: 6, should print:
+            //
+            // 3
+            // 4
+            // 5
+        }
+        static void Comparing(int Number1, int Number2)
+        {
+
+        }
+        static void Counting(int Number1, int Number2)
+        {
+
+        }
+    }
+}
