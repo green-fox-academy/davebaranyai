7338ead4faab1206287c9aab034bc59ee3744960
davebaranyai <davebaranyai@gmail.com>
2017-12-14 17:16:16 +0100

    feat(day2_Lists02): exercise done

---
+++ week-02/day-3/Lists02/Lists02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matchmaking
+{
+    public class Matchmaking
+    {
+        public static void Main(string[] args)
+        {
+            var girls = new List<string> { "Eve", "Ashley", "Bözsi", "Kat", "Jane" };
+            var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
+            var order = new List<string>();
+
+            for (int i = 0; i < girls.Count; i++)
+            {
+                order.Add(girls[i]);
+                order.Add(boys[i]);
+            }
+
+            foreach (string member in order)
+            {
+
+                Console.WriteLine(member);
+
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
