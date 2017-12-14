3e9f45fa4aabf6d173a9025783a91d73751a178f
davebaranyai <davebaranyai@gmail.com>
2017-12-14 16:07:52 +0100

    feat(day2_PrintElements): exercise done

---
+++ week-02/day-2/PrintElements/PrintElements/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PrintElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `af`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print all the elements of `af`
+
+            int[] af = { 4, 5, 6, 7 };
+
+            for (int i = 0; i < af.Length; i++)
+            {
+                Console.WriteLine(af[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
