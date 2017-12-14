31e8b021bbeff898587500bd9152f313dff2e94f
davebaranyai <davebaranyai@gmail.com>
2017-12-14 15:31:05 +0100

    feat(day2_CompareLength): exercise done

---
+++ week-02/day-2/CompareLength/CompareLength/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CompareLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] p1 = { 1, 2, 3 };
+            int[] p2 = { 4, 5, };
+
+            if (p1.Length < p2.Length)
+            {
+                Console.WriteLine("Of course it has more elements!");                 
+            }
+            else
+            {
+                Console.WriteLine("Nah, it sure does not have more elements!");
+            }
+            Console.ReadLine();
+
+            // - Create an array variable named `p1`
+            //   with the following content: `[1, 2, 3]`
+            // - Create an array variable named `p2`
+            //   with the following content: `[4, 5]`
+            // - Print if `p2` has more elements than `p1`
+        }
+    }
+}
