91194ab4deab38848d7bba491ccc90687e3db2e3
davebaranyai <davebaranyai@gmail.com>
2017-12-14 16:56:59 +0100

    feat(day2_SwapElements): exercise done

---
+++ week-02/day-2/SwapElements/SwapElements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SwapElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `abc`
+            //   with the following content: `["first", "second", "third"]`
+            // - Swap the first and the third element of `abc`
+
+            string[] abc = { "first", "second", "third" };
+
+            string temporary = abc[2];
+            abc[2] = abc[0];
+            abc[0] = temporary;
+
+
+            for (int i = 0; i < abc.Length; i++)
+            {
+                Console.Write((abc[i]) + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
