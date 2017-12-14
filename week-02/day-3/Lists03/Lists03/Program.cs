17043c1b5c1a182afac837ff1a310b312a0a37f2
davebaranyai <davebaranyai@gmail.com>
2017-12-14 17:33:54 +0100

    feat(day2_Lists03): exercise done

---
+++ week-02/day-3/Lists03/Lists03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppendLetter
+{
+    public class AppendLetter
+    {
+
+        public static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            // Add "a" to every string in the far list.
+            foreach (string nimals in far)
+            {
+                var animals = new StringBuilder();
+                animals.Append(nimals);
+                animals.Append("a");
+
+                Console.WriteLine(animals);
+            }
+            Console.ReadLine();
+        }
+    }
+}
