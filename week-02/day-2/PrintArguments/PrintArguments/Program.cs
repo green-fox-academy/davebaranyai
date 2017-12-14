1431ccd09d05625107273e816109a7d195fa2b44
davebaranyai <davebaranyai@gmail.com>
2017-12-14 15:04:32 +0100

    feat(day2_PrintArguments): project created, basic structure developed

---
+++ week-02/day-2/PrintArguments/PrintArguments/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrintArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
