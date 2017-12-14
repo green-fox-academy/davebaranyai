ca609ea2b725a8f37936bce57cf2f524b68213f8
davebaranyai <davebaranyai@gmail.com>
2017-12-14 01:00:06 +0100

    feat(day1_Exercise27): main method done, added FizzerBuzzer method

---
+++ week-02/day-1/Exercise27/Exercise27/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MaxNumber = 100;
+
+            FizzBuzzer(MaxNumber);
+            Console.ReadLine();
+
+            // Write a program that prints the numbers from 1 to 100.
+            // But for multiples of three print “Fizz” instead of the number
+            // and for the multiples of five print “Buzz”.
+            // For numbers which are multiples of both three and five print “FizzBuzz”.
+        }
+        static void FizzBuzzer(int MaxNumber)
+        {
+
+        }
+    }
+}
