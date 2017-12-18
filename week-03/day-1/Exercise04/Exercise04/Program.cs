5ec5b02c772e7805677ebee3000397015a09bd61
davebaranyai <davebaranyai@gmail.com>
2017-12-18 17:32:16 +0100

    feat(week3_day1_exercise04): exercise done

---
+++ week-03/day-1/Exercise04/Exercise04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @".\My-File.txt";
+            FileWriter(fileName);
+        }
+        static void FileWriter(string fileName)
+        {
+            try
+            {
+                File.WriteAllText(fileName, "Baranyai David");
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine("Unable to write file: my-file.txt");
+                Console.Read();                
+            }            
+        }
+    }
+}
