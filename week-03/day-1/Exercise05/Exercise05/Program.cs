5f1e518cdf7762c621c661accdef30279fc405ad
davebaranyai <davebaranyai@gmail.com>
2017-12-18 23:25:13 +0100

    feat(week3_day1_exercise05): exercise done

--- week-03/day-1/Exercise05/Exercise05/Program.cs
+++ week-03/day-1/Exercise05/Exercise05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Exercise05
 {
@@ -6,19 +7,36 @@ namespace Exercise05
     {
         static void Main(string[] args)
         {
-            // Create a function that takes 3 parameters: a path, a word and a number,
-            // than it should write to a file.
-            // The path parameter should be a string, that describes the location of the file.
-            // The word parameter should be a string, that will be written to the file as lines
-            // The number paramter should describe how many lines the file should have.
-            // So if the word is "apple" and the number is 5, than it should write 5 lines
-            // to the file and each line should be "apple"
-            // The function should not raise any error if it could not write the file.
+            string path = @"./TextFile1.txt";
+            UserInput(path);
         }
-        static void FileWriter()
+
+        static void UserInput(string path)
         {
-
+            Console.WriteLine("What is the word?");
+            string word = Console.ReadLine();
+            Console.WriteLine("How many lines?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            FileWriter(path, word, number);
         }
 
+        static void FileWriter(string path, string word, int number)
+        {
+            try
+            {
+                string[] allStrings = new string[number];
+                for (int i = 0; i < (number); i++)
+                {
+                    allStrings[i] = word;
+                }
+                File.WriteAllLines(path, allStrings);
+                Console.WriteLine("File updated!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);                
+            }
+            Console.ReadLine();
+        }
     }
 }
