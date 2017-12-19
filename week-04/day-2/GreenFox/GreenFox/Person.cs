fad7a509ef23f971e381cfd01db116fd931937a5
davebaranyai <davebaranyai@gmail.com>
2017-12-19 17:29:28 +0100

    feat(week4_day2_GreenFox): buils up Person and Student classes

--- week-04/day-2/GreenFox/GreenFox/Person.cs
+++ week-04/day-2/GreenFox/GreenFox/Person.cs
@@ -6,5 +6,25 @@ namespace GreenFox
 {
     class Person
     {
+        public string name;
+        public int age;
+        public string gender;
+
+        public Person()
+        {
+            name = "Jane Doe";
+            age = 30;
+            gender = "female";
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hi, I'm " + name + ", a(n) " + age + " year old " + gender + ".");
+        }
+
+        public void GetGoal()
+        {
+            Console.WriteLine(@"My goal is: Live for the moment!");
+        }
     }
 }
--- week-04/day-2/GreenFox/GreenFox/Student.cs
+++ week-04/day-2/GreenFox/GreenFox/Student.cs
@@ -4,7 +4,31 @@ using System.Text;
 
 namespace GreenFox
 {
-    class Student
+    class Student : Person
     {
+        string previousOrganization;
+        int skippedDays;
+
+        public Student(string previousOrganization, int skippedDays)
+        {
+            previousOrganization = "The School of Life";
+            skippedDays = 0;
+        }
+
+        public new void GetGoal()
+        {
+            Console.WriteLine("Be a junior software developer.");
+        }
+
+        public new void Introduce()
+        {
+            Console.WriteLine("Hi, I'm " + name + ",a(n) " + age + " year old " + gender + " from " + previousOrganization + " who skipped " + skippedDays + " days from the course already.");
+        }
+
+        public void SkipDays(int numberOfDays, int SkippedDays)
+        {
+            int skippedDays = SkippedDays + numberOfDays;
+        }
+
     }
 }
