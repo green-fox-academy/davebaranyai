5bf4ca82181fe5b37d64f1ada7a8f4c2c24df114
davebaranyai <davebaranyai@gmail.com>
2017-12-21 16:25:10 +0100

    feat(week4_gardening): exercise done

---
+++ week-04/day-2/Garden/Garden/Flower.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Garden
+{
+    class Flower : Plant
+    {
+        public Flower(string color) : base(color)
+        {
+            Treshold = 5;
+            AbsorbRate = 0.75;
+        }
+        public override void AbsorbWater(double amount)
+        {
+            CurrentWater += amount * AbsorbRate;
+        }
+    }
+}
---
+++ week-04/day-2/Garden/Garden/Tree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Garden
+{
+    class Tree : Plant
+    {
+        public Tree(string color) : base(color)
+        {
+            Treshold = 10;
+            AbsorbRate = 0.4;
+        }
+        public override void AbsorbWater(double amount)
+        {
+            CurrentWater += amount * AbsorbRate;
+        }
+    }
+}
