8e23e92aabc6a26dd768d974c7153ffd5d2b0a61
davebaranyai <davebaranyai@gmail.com>
2017-12-22 00:14:36 +0100

    feat(week4_sum): create program class for practicing classes, add all parameters needed, create method for summing

---
+++ week-04/day-3/Sum/Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberList = new List<int> { 12, 23, 45 };
+            
+            Console.WriteLine(ListSummer(numberList));
+            Console.ReadLine();
+        }
+
+        static int ListSummer(List<int> numberList)
+        {
+            int sumOfNumbers = 0;
+
+            foreach (var item in numberList)
+            {
+                sumOfNumbers += item;
+            }
+
+            return sumOfNumbers;
+        }
+    }
+}
