f2ab99b33d8d5e79c8adbcb6db929f83fd58aed1
davebaranyai <davebaranyai@gmail.com>
2018-01-02 12:14:50 +0100

    feat(Cloneable): copied greenfox task from week4

---
+++ week-06/Cloneable/Cloneable/PallidaClass.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Cloneable
+{
+    public class PallidaClass
+    {
+        public string ClassName { get; set; }
+        public List<Student> Students { get; set; }
+        public List<Mentor> Mentors { get; set; }
+
+        public PallidaClass(string className)
+        {
+            ClassName = className;
+            Students = new List<Student>();
+            Mentors = new List<Mentor>();
+        }
+
+        public void AddStudent(Student student)
+        {
+            if (!Students.Contains(student))
+            {
+                Students.Add(student);
+            }
+        }
+        public void AddMentor(Mentor mentor)
+        {
+            if (!Mentors.Contains(mentor))
+            {
+                Mentors.Add(mentor);
+            }
+        }
+
+        public void Info()
+        {
+            System.Console.WriteLine("Corsac {0} class has {1} students and {2} mentors.", ClassName, Students.Count, Mentors.Count);
+        }
+    }
+}
