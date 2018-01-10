28694dbdb866fb3f3bcddcf3888420a9c88d6a2f
davebaranyai <davebaranyai@gmail.com>
2018-01-10 17:29:52 +0100

    feat(Task01): add home and REST controller, finish 'hello world REST app' part

---
+++ week-07/day-1/Task01/Task01/Controllers/RESTController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Task01.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Task01.Controllers
+{    
+    public class RESTController : Controller
+    {
+        [Route("api")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("greeting")]
+        public IActionResult Greeting([FromQuery] string name)
+        {
+            var greeting = new Greeting();           
+            greeting.Content = "Hello," + name;
+            return new JsonResult(greeting);
+        }
+
+    }
+}
