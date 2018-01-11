7e950bc6d25dfef5e89f1ab7768eae1e6b79a994
davebaranyai <davebaranyai@gmail.com>
2018-01-11 14:18:34 +0100

    feat(BookKeeperProject): add HomeController to Controllers and ListOfBooks to Models, delete comments from startup.cs, add properties to ListOfBooks

---
+++ week-07/day-3/BookKeeperProject/BookKeeperProject/Models/ListOfBooks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookKeeperProject.Models
+{
+    public class ListOfBooks
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorFirstName { get; set; }
+        public string AuthorLastName { get; set; }
+        public int Year { get; set; }
+        public string Type { get; set; }
+        public string Location { get; set; }
+        public bool Availability { get; set; }
+    }
+}
