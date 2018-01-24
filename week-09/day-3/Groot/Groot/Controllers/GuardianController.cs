5211cda85801f895c8a092daf549b682598c7738
davebaranyai <davebaranyai@gmail.com>
2018-01-24 14:27:54 +0100

    feat(Groot): add guardiancontroller based on task description

---
+++ week-09/day-3/Groot/Groot/Controllers/GuardianController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Groot.Controllers
+{
+    public class GuardianController : Controller
+    {
+        [HttpGet("groot")]
+        public IActionResult Index(string message)
+        {
+            if (message == null)
+            {
+                return Json(new { error = "I am Groot!" });
+            }
+
+            return Json(new { received = message, translated = "I am Groot!" });
+        }
+    }
+}
