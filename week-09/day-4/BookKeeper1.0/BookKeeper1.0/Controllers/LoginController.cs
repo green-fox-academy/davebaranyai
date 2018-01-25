0473261207c1bbc612ed654d1e0ab93a3ca93920
davebaranyai <davebaranyai@gmail.com>
2018-01-25 12:05:05 +0100

    feat(BookKeeper1.0): update mvc folder structure, add context file, add controllers

---
+++ week-09/day-4/BookKeeper1.0/BookKeeper1.0/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookKeeper.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("")]
+        public IActionResult Login(string name)
+        {
+            if (name.Equals("David"))
+            {
+                return Redirect("home");
+            }
+            else if (name.Equals("Lilla"))
+            {
+                return Redirect("home");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
