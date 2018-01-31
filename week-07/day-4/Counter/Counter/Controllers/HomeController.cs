6b30ca5e269e2ef4c4d8896d8a3facb97a45b9a8
davebaranyai <davebaranyai@gmail.com>
2018-01-31 11:35:23 +0100

    feat(w07_d4_Counter): create endpoints, add index view

---
+++ week-07/day-4/Counter/Counter/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Counter.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
