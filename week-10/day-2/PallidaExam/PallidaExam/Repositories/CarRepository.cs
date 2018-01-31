c9c88eeac3609876c7a1101d6da7574d02bc7a40
davebaranyai <davebaranyai@gmail.com>
2018-01-31 20:43:15 +0100

    feat(PallidaExam): add CarRepository and CarService, set up DI structure throughout the project, set up default route in the controller

---
+++ week-10/day-2/PallidaExam/PallidaExam/Repositories/CarRepository.cs
@@ -0,0 +1,21 @@
+﻿using PallidaExam.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PallidaExam.Repositories
+{
+    public class CarRepository
+    {
+        private CarContext carContext;
+
+        public CarRepository(CarContext carContext)
+        {
+            this.carContext = carContext;
+        }
+
+
+	//ide johetnek pl a listak
+    }
+}
---
+++ week-10/day-2/PallidaExam/PallidaExam/Services/CarService.cs
@@ -0,0 +1,21 @@
+﻿using PallidaExam.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PallidaExam.Services
+{
+    public class CarService
+    {
+        private CarRepository carRepository;
+
+        public CarService(CarRepository carRepository)
+        {
+            this.carRepository = carRepository;
+        }
+
+
+        //ide jonnek a bindzsizesek
+    }
+}
