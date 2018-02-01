9ff834bee04bb5d8cc4ce3e7830b76ea6b8b3c5c
davebaranyai <davebaranyai@gmail.com>
2018-02-01 12:43:27 +0100

    feat(w09_d1_Doubling): add RESTController with doubling method

---
+++ week-09/day-1/Doubling/Doubling/Controllers/RESTController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Doubling.Controllers
+{
+    [Produces("application/json")]
+    [Route("api")]
+    public class RESTController : Controller
+    {
+        [HttpGet("doubling")]
+        public IActionResult Doubling([FromQuery]int? input)
+        {
+            if (input == null)
+            {
+                return Json(new { error = "Please provide an input!" });
+            }
+            return Json(new { received = input, result = input * 2 });
+        }
+
+    }
+}
