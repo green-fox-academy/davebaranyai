d700b8d3cd8f22f791f8870ca3d46198b4ac8cb1
davebaranyai <davebaranyai@gmail.com>
2018-02-01 14:57:56 +0100

    feat(PallidaRetake): add Item model

---
+++ week-10/day-4/PallidaRetake/PallidaRetake/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PallidaRetake.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public string Manufacturer { get; set; }
+        public string Category { get; set; }
+        public string Size { get; set; }
+        public float UnitPrice { get; set; }
+    }
+}
