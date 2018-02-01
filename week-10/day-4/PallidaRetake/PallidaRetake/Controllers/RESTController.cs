851ca3e561723a76e135a0f7a6e99f12f210246f
davebaranyai <davebaranyai@gmail.com>
2018-02-01 14:52:09 +0100

    feat(PallidaRetake): add WarehouseController and RESTController, set up default route for both controllers

---
+++ week-10/day-4/PallidaRetake/PallidaRetake/Controllers/RESTController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PallidaRetake.Controllers
+{
+    [Produces("application/json")]
+    [Route("warehouse/query")]
+    public class RESTController : Controller
+    {
+    }
+}
---
+++ week-10/day-4/PallidaRetake/PallidaRetake/Controllers/WarehouseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PallidaRetake.Controllers
+{
+    [Route("")]
+    public class WarehouseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
