59f263bc647c7e503623de69785344547350cb5f
davebaranyai <davebaranyai@gmail.com>
2018-02-01 22:37:46 +0100

    feat(Yondu): create appropriate controller, base app done

---
+++ week-09/day-3/Yondu/Yondu/Controllers/RESTController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Yondu.Controllers
+{
+    [Produces("application/json")]
+    [Route("")]
+    public class RESTController : Controller
+    {
+        [HttpGet("yondu")]
+        public IActionResult SpeedCalculator([FromQuery]double distance, double time)
+        {
+            return Json(new { Distance = distance, Time = time, Speed = (distance/time)});
+        }
+    }
+}
