4a41aa48f1a01dd2a2148a2e0208cc7f43d97b69
davebaranyai <davebaranyai@gmail.com>
2018-03-26 09:24:12 +0200

    feat(palindrome): add question in console writeline, and save user input into a string

---
+++ final-exam/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a word:");
+            string input = Console.ReadLine();
+        }
+    }
+}
